f54611215dcec3e0380627bb23443d779646818c
coco11563 <coco11563@yeah.net>
2018-11-12 11:21:09 +0800

    finished the multiThread part
    now write the string builder part

--- src/main/scala/datastructure/NodeTreeHandler.scala
+++ src/main/scala/datastructure/NodeTreeHandler.scala
@@ -1,6 +1,7 @@
 package datastructure
 
-import java.util.concurrent.Callable
+import java.util.concurrent._
+import java.util.concurrent.locks.LockSupport
 
 import datastructure.Obj.Property
 import datastructure.Obj.TypeMap.{B, L, U}
@@ -23,8 +24,8 @@ class NodeTreeHandler extends RDFHandler with Callable[Int]{
   val BNodeMap : mutable.HashMap[BNode, mutable.Set[Node]] = new mutable.HashMap[BNode, mutable.Set[Node]]()
   val PropNames : mutable.HashSet[Property] = new mutable.HashSet[Property]()
   val relationQueue : mutable.Queue[Statement] = new mutable.Queue[Statement]()
-  val unhandleBNodeTail : mutable.Queue[Statement] = new mutable.Queue[Statement]()
-
+  val unHandleBNodeTail : mutable.Queue[Statement] = new mutable.Queue[Statement]()
+  lazy val DEFAULT: ThreadPoolExecutor = NodeTreeHandler.createDefaultPool
   override def handleStatement(statement: Statement) : Unit = {
     handleStatement(statement.getSubject, statement.getPredicate, statement.getObject, statement)
   }
@@ -61,28 +62,33 @@ class NodeTreeHandler extends RDFHandler with Callable[Int]{
       else BNodeMap.put(c, mutable.Set(node))
     case (a:B, b:U, c:L) =>
       if (BNodeMap.contains(a)) BNodeMap(a).foreach(n => n.handle(statement))
-      else unhandleBNodeTail.enqueue(statement)
+      else unHandleBNodeTail.enqueue(statement)
     case (a:B, b:U, c:U) =>
       if (BNodeMap.contains(a)) BNodeMap(a).foreach(n => n.handle(statement))
-      else unhandleBNodeTail.enqueue(statement)
+      else unHandleBNodeTail.enqueue(statement)
     case (a:B, b:U, c:B) =>
       if (BNodeMap.contains(a)) BNodeMap(a).foreach(n => {
         n.handle(statement)
         if(BNodeMap.contains(c)) BNodeMap(c).add(n)
         else BNodeMap.put(c,mutable.Set(n))
       })
-      else unhandleBNodeTail.enqueue(statement)
+      else unHandleBNodeTail.enqueue(statement)
     case _ =>
   }
 
   override def startRDF(): Unit = {}
 
-  override def endRDF(): Unit = {}
+  override def endRDF(): Unit = {
+    val nums = NodeTreeHandler.intoFuture[Int](DEFAULT,this)
+    while (!nums.isDone) {}
+    println(nums.get())
+  }
 
   override def handleNamespace(prefix: String, uri: String): Unit = {}
 
   override def handleComment(comment: String): Unit = {}
 
+  def getAllNode: Iterable[Node] = NodeMap.values
   /**
     * this call method will be called after all normal node processed
     *
@@ -99,6 +105,67 @@ class NodeTreeHandler extends RDFHandler with Callable[Int]{
     * @return the processed statements num
     */
   override def call(): Int = {
-
+    var count = 0 // processed statements count
+    var perCount = 0 // this iterate's process statements count
+    var done = false
+    while (unHandleBNodeTail.nonEmpty && !done) {
+      val statements : mutable.Seq[Statement] = unHandleBNodeTail.dequeueAll(_ => true) // dequeue all
+      statements.foreach(
+        statement => {
+          val subject = statement.getSubject
+          val obj = statement.getObject
+          val predicate = statement.getPredicate
+          (subject, obj, predicate) match {
+            case (a:B, b:U, c:L) =>
+              if (BNodeMap.contains(a)) {BNodeMap(a).foreach(n => n.handle(statement)) ; count += 1; perCount += 1}
+              else unHandleBNodeTail.enqueue(statement)
+            case (a:B, b:U, c:U) =>
+              if (BNodeMap.contains(a)) {BNodeMap(a).foreach(n => n.handle(statement)) ; count += 1; perCount += 1}
+              else unHandleBNodeTail.enqueue(statement)
+            case (a:B, b:U, c:B) =>
+              if (BNodeMap.contains(a)) BNodeMap(a).foreach(n => {
+                n.handle(statement)
+                if(BNodeMap.contains(c)) BNodeMap(c).add(n)
+                else BNodeMap.put(c,mutable.Set(n))
+                count += 1; perCount += 1
+              })
+              else unHandleBNodeTail.enqueue(statement)
+          }
+        })
+      if (perCount == 0) done = true
+      else perCount = 0
+    }
+    count
   }
+}
+
+object NodeTreeHandler {
+  def intoFuture[T](pool: ExecutorService , callable: Callable[T]): Future[T] = {
+      pool.submit(() => {
+        def foo() = try {
+          callable.call
+        }
+        foo()
+      })
+  }
+    def createDefaultPool :ThreadPoolExecutor = {
+      val threads = Runtime.getRuntime.availableProcessors * 2
+      val queueSize = threads * 25
+      new ThreadPoolExecutor(threads / 2, threads, 30L, TimeUnit.SECONDS, new ArrayBlockingQueue[Runnable](queueSize), new CallerBlocksPolicy)
+      //                new ThreadPoolExecutor.CallerRunsPolicy());
+    }
+
+    class CallerBlocksPolicy() extends RejectedExecutionHandler {
+      override def rejectedExecution(r: Runnable, executor: ThreadPoolExecutor): Unit = {
+        if (!executor.isShutdown) { // block caller for 100ns
+          LockSupport.parkNanos(100)
+          try // submit again
+          executor.submit(r).get
+          catch {
+            case e@(_: InterruptedException | _: ExecutionException) =>
+              throw new RuntimeException(e)
+          }
+        }
+      }
+    }
 }
